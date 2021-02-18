c836c1ca769bc0f6cd05b6908e6c360fe4f62dd1
Kathryn Wilkinson <kathrynxwilk@gmail.com>
2021-02-18 15:23:18 -0600

    Initial commit

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+prompt("what is your name?");
+prompt("What is their name?");
+
+var loveScore = Math.random() * 100;
+loveScore = Math.floor(loveScore) +1; //+1 bc the Math.floor method counts up to the number specified, but doesn't include it.
+alert("Your love score is " + loveScore + "%");
