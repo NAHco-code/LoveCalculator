dee5b5a6835576e3be4ee7d9eaaaef81f35c96aa
Kathryn Wilkinson <kathrynxwilk@gmail.com>
2021-02-18 23:35:07 -0600

    Doc: edit prompts

--- assets/script.js
+++ assets/script.js
@@ -2,5 +2,11 @@ prompt("what is your name?");
 prompt("What is their name?");
 
 var loveScore = Math.random() * 100;
-loveScore = Math.floor(loveScore) +1; //+1 bc the Math.floor method counts up to the number specified, but doesn't include it.
-alert("Your love score is " + loveScore + "%");
+loveScore = Math.floor(loveScore) +1; //1-100 // +1 bc Math.floor method counts up to the number specified, but doesn't include it.
+
+ if (loveScore > 75) {
+     alert("Your love score is " + loveScore + "%" + "! You love eachother like Kanye loves Kanye.");
+ } else {
+     alert("Your love score is " + loveScore + "%");
+ }
+
